9853876e6d6ce304c441dd8ca11e69592a180982
Nicholas Bertocco <nicholas.h.bertocco@gmail.com>
2025-01-27 12:19:24 -0300

    Atualizacao-dos-cards
    * Classifiquei os elementos por elemento alterando o estilo de cada elemento e adicionando um icone
    *Ajustei o CSS para que fique mais responsivo

--- src/app/componentes/Card/index.jsx
+++ src/app/componentes/Card/index.jsx
@@ -1,12 +1,35 @@
 import Image from 'next/image';
 import styles from './Card.module.css';
+import { PiPlant } from "react-icons/pi";
+import { MdAir } from "react-icons/md";
+import { FaFire, FaWater } from "react-icons/fa";
 
 export default function Card (props){
     return(
-        <div className={styles.Card}>
-            <h3>{props.signo}</h3>
+        <div className={
+            props.elemento === "agua"
+            ? styles.CardAgua
+            : props.elemento === "fogo"
+            ? styles.CardFogo
+            : props.elemento === "terra"
+            ? styles.CardTerra
+            : props.elemento === "ar" && styles.CardAr
+        }>
+            <div className={styles.boxTitleIcon}>
+                <h3>{props.signo}</h3>
+                {props.elemento ==="agua" ? (
+                    <FaWater className={styles.icone}/>
+                ) : props.elemento === "fogo" ? (
+                    <FaFire className={styles.icone} />
+                ) : props.elemento ==="terra" ? (
+                    <PiPlant className={styles.icone}/>
+                ) : props.elemento ==="ar" && (
+                    <MdAir className={styles.icone}/>
+                )}
+            </div>
+        
             <Image src={props.imagem} alt={props.signo}/>
             <span>{props.data}</span>
         </div>
-    )
-};
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -20,18 +20,18 @@ export default function Home() {
       <Topo />
       <main className={styles.conteinerMain}>
         <section className={styles.secaoBaralho}>
-          <Card signo={'Aquário'} imagem={Aquario} data={'21/01 - 19/02'} />
-          <Card signo={'Peixes'} imagem={Peixes} data={'20/02 - 20/03'}/>
-          <Card signo={'Áries'} imagem={Aries} data={'21/03 - 20/04'} />
-          <Card signo={'Touro'} imagem={Touro} data={'21/04 - 21/05'}  />
-          <Card signo={'Gêmeos'} imagem={Gemeos} data={'22/05 - 21/06'} />
-          <Card signo={'Câncer'} imagem={Cancer} data={'22/06 - 23/07'} />
-          <Card signo={'Leão'} imagem={Leao} data={'24/07 - 23/08'} />
-          <Card signo={'Virgem'} imagem={Virgem} data={'24/08 - 23/09'} />
-          <Card signo={'Libra'} imagem={Libra} data={'24/09 - 23/10'} />
-          <Card signo={'Escorpião'} imagem={Escorpiao} data={'24/10 - 22/11'} />
-          <Card signo={'Sagitário'} imagem={Sagitario} data={'23/11 - 21/12'} />
-          <Card signo={'Capricórnio'} imagem={Capricornio} data={'22/12 - 20/01'} />
+          <Card elemento={'ar'} signo={'Aquário'} imagem={Aquario} data={'21/01 - 19/02'} />
+          <Card elemento={ 'agua'} signo={'Peixes'} imagem={Peixes} data={'20/02 - 20/03'}/>
+          <Card elemento={ 'fogo'} signo={'Áries'} imagem={Aries} data={'21/03 - 20/04'} />
+          <Card elemento={ 'terra'} signo={'Touro'} imagem={Touro} data={'21/04 - 21/05'}  />
+          <Card elemento={ 'ar'} signo={'Gêmeos'} imagem={Gemeos} data={'22/05 - 21/06'} />
+          <Card elemento={ 'agua'} signo={'Câncer'} imagem={Cancer} data={'22/06 - 23/07'} />
+          <Card elemento={ 'fogo'} signo={'Leão'} imagem={Leao} data={'24/07 - 23/08'} />
+          <Card elemento={ 'terra'} signo={'Virgem'} imagem={Virgem} data={'24/08 - 23/09'} />
+          <Card elemento={ 'ar'} signo={'Libra'} imagem={Libra} data={'24/09 - 23/10'} />
+          <Card elemento={ 'agua'} signo={'Escorpião'} imagem={Escorpiao} data={'24/10 - 22/11'} />
+          <Card elemento={ 'fogo'} signo={'Sagitário'} imagem={Sagitario} data={'23/11 - 21/12'} />
+          <Card elemento={ 'terra'} signo={'Capricórnio'} imagem={Capricornio} data={'22/12 - 20/01'} />
         </section>
       </main>
     </div>
